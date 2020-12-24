194b8adf41d4a4c882491f769b04379f5994b67c
Le Tran Ngoc Dat <62195637+ltndat99@users.noreply.github.com>
2020-12-24 12:13:53 +0700

    Install prettier, lint-staged, husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-const newsRouter = require("./news");
-const siteRouter = require("./site");
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 
 // Action --> Dispatcher --> Function Handler
 
 function route(app) {
-  app.use("/news", newsRouter);
-  app.use("/", siteRouter);
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
 }
 
 module.exports = route;
